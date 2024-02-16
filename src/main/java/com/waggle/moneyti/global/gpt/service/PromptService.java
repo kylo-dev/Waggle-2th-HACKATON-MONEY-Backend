a0ee0b270e2f0d5e86c3c7bc7c9bb942adb4ddc1
gyeom <rlagusrua3687@gmail.com>
2024-02-17 03:19:54 +0900

    :recycle: refactor: PromptService 리팩토링

--- src/main/java/com/waggle/moneyti/global/gpt/service/PromptService.java
+++ src/main/java/com/waggle/moneyti/global/gpt/service/PromptService.java
@@ -2,16 +2,12 @@ package com.waggle.moneyti.global.gpt.service;
 
 import com.waggle.moneyti.domain.MoneyTI.MoneyTI;
 import com.waggle.moneyti.domain.MoneyTI.repository.MoneyTIRepository;
-import com.waggle.moneyti.domain.Recommend.Recommend;
-import com.waggle.moneyti.domain.Recommend.repository.RecommendRepository;
 import com.waggle.moneyti.global.config.ChatGPTConfig;
 import com.waggle.moneyti.global.gpt.dto.ChatMessage;
 import com.waggle.moneyti.global.gpt.dto.ChatRequest;
 import com.waggle.moneyti.global.gpt.dto.ChatResponse;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,37 +30,11 @@ public class PromptService {
     @Value("${chatgpt.url}")
     private String url;
 
-//    public String prompt(String prompt) {
-//
-//        // 토큰 정보가 포함된 Header 가져오기
-//        HttpHeaders headers = chatGPTConfig.httpHeaders();
-//
-//        // Create request
-//        ChatRequest chatRequest = new ChatRequest(model, prompt);
-//
-//        // 통신을 위한 RestTemplate 구성하기
-//        HttpEntity<ChatRequest> requestEntity = new HttpEntity<>(chatRequest, headers);
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        ChatResponse response = restTemplate.postForObject(url, requestEntity, ChatResponse.class);
-//
-//        if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
-//            throw new RuntimeException();
-//        }
-//
-//        return response.getChoices().get(0).getMessage().getContent();
-//    }
-
     public String prompt(Long moneyTIId) {
         // 토큰 정보가 포함된 Header 가져오기
         HttpHeaders headers = chatGPTConfig.httpHeaders();
 
         // Create request
-//        ChatRequest chatRequest = new ChatRequest(model, prompt);
-//
-//        // 통신을 위한 RestTemplate 구성하기
-//        HttpEntity<ChatRequest> requestEntity = new HttpEntity<>(chatRequest, headers);
-
         ChatRequest chatRequest = generateChatPrompt(moneyTIId);
         HttpEntity<ChatRequest> requestEntity = new HttpEntity<>(chatRequest, headers);
 
@@ -74,9 +44,6 @@ public class PromptService {
         if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
             throw new RuntimeException();
         }
-
-        log.info("========= response : " + response.toString());
-
         return response.getChoices().get(0).getMessage().getContent();
     }
 
@@ -101,10 +68,10 @@ public class PromptService {
         messages.add(ChatMessage.builder()
             .role("system")
             .content(
-                "우리는 당신의 선택에 가장 잘 맞는 금융상품과 소비습관을 추천하는 임무를 맡고 있습니다." +
-                    "고객이 명확한 목표를 가지고 있는지, 지출이 많은지, 미래 지향적인지, 현재 지향적인지 등을 알고 있습니다."+
-                    "당신은 결과 추천으로 \"소비 습관\"과 \"추천 상품\"의 두 가지 구성 요로 JSON 형식으로 추천합니다."+
-                    " \"소비습관\"을 키워드로 2개의 단어를 적으며 \"추천 상품\"은 간략하게 1, 2줄의 설명으로 구성되어 있습니다" )
+                "당신은 고객의 특성을 분석하여 가장 잘 맞는 금융상품과 소비습관을 추천하는 임무를 맡고 있습니다." +
+                    "당신은 결과 추천으로 \"RecommendHabit\"과 \"RecommendProduct\"의 두 가지 구성 요로 JSON 형식으로 추천합니다."
+                    +
+                    " \"소비습관\", \"추천 상품\"을 간략하게 2줄로 추천과 설명의 글을 작성해줘.")
             .build());
 
         //GPT에 유저 정보 제공 (Context)
@@ -120,12 +87,8 @@ public class PromptService {
         //GPT에 질문 (작업)
         messages.add(ChatMessage.builder()
             .role("user")
-            .content("나에게 맞는 금융상품과 스마트한 소비습관을 추천해주시고 1-2문장으로 간략하게 JSON으로 설명해주세요.")
+            .content("나에게 맞는 금융상품과 현명한 소비습관을 추천해주고 JSON 형식으로 반환해주세요.")
             .build());
-
-//        Map<String, Object> prompt = new HashMap<>();
-//        prompt.put("model", model);
-//        prompt.put("messages", messages);
 
         ChatRequest chatRequest = new ChatRequest(model, messages);
 
@@ -139,9 +102,9 @@ public class PromptService {
         String[] contents = content.split(",");
 
         //순서:
-        String userInfo = String.format("\"Users are \"%s\""
-                + "\"Users are \"%s\""
-                + "\"Users are \"%s\""
+        String userInfo = String.format("\"사용자의 특징은 \"%s\""
+                + ", \"%s\""
+                + ", \"%s\" 사람이라고 할 수 있어."
             , contents[0], contents[1], contents[2]);
 
         log.info("=========== usrInfo : " + userInfo);
